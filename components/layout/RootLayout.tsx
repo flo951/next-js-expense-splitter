908ab42310e327026fefe8c2b30f257284584690
Florian Görlich <florian.goerlich95@gmail.com>
2026-02-08 10:01:14 +0100

    Add sonar analysis to build step

--- components/layout/RootLayout.tsx
+++ components/layout/RootLayout.tsx
@@ -2,7 +2,7 @@
 
 import type { ReactNode } from 'react'
 import type { User } from '@/types'
-import Header from './header'
+import Header from "@/components/layout/Header";
 
 type RootLayoutProps = {
   children: ReactNode
